0349b020ed7505cad8cf4cfc9b762ba891794aaa
Naja Aldefri <naja.aldefri@gmail.com>
2019-01-10 14:18:51 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ require_relative '../say_hello'
 RSpec.configure do |config|
   # config here
 end
+describe say_hello(name)
+puts "name"
+end
+def say_hello(name = "Naja")
+  puts "Hello, #{name}"
+end
