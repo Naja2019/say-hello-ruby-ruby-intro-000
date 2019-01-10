355151ef6c141720fa4d2a9fe855980995fcc288
Naja Aldefri <naja.aldefri@gmail.com>
2019-01-10 14:18:44 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -11,6 +11,4 @@ describe "say_hello" do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
   end
-  def say_hello(name = "Naja")
-    puts "Hello, #{name}"
-  end
+  
